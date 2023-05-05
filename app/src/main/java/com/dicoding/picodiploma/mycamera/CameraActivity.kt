208bdd41198df8a5e02533ea97e000026170437a
heireev <muhammadrifqi216@gmail.com>
2023-05-05 14:09:01 +0800

    [problem] Mengambil Gambar dari Kamera

--- app/src/main/java/com/dicoding/picodiploma/mycamera/CameraActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/CameraActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
@@ -37,7 +38,32 @@ class CameraActivity : AppCompatActivity() {
     }
 
     private fun takePhoto() {
-       // takePhoto
+        val imageCapture = imageCapture ?: return
+
+        val photoFile = createFile(application)
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile)
+            imageCapture.takePicture(
+                outputOptions,
+                ContextCompat.getMainExecutor(this),
+                object : ImageCapture.OnImageCapturedCallback {
+                    override fun onError(exc: ImageCaptureException) {
+                        Toast.makeText(
+                            this@CameraActivity,
+                            "Gagal mengambil gambar.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                        Toast.makeText(
+                            this@CameraActivity,
+                            "Berhasil mengambil gambar.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            )
     }
 
     private var imageCapture: ImageCapture? = null
